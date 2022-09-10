00dd725d3b2322c2de3d8eb34ed2c964a3418caa
MSE5 <70346692+MSE5@users.noreply.github.com>
2022-09-10 17:55:01 +0800

    Update main-w.py

--- main-w.py
+++ main-w.py
@@ -331,7 +331,7 @@ def push(body):
         print('*** No BARK_KEY ***')
     else:
         barkurl = 'https://api.day.app/' + BARK_KEY
-        title = 'H-Extend'
+        title = 'W-Extend'
         rq_bark = requests.get(url=f'{barkurl}/{title}/{body}?isArchive=1')
         if rq_bark.status_code == 200:
             print('- bark push Done!')
@@ -341,7 +341,7 @@ def push(body):
     if TG_BOT_TOKEN == '' or TG_USER_ID == '':
         print('*** No TG_BOT_TOKEN or TG_USER_ID ***')
     else:
-        body = 'H-Extend\n\n' + body
+        body = 'W-Extend\n\n' + body
         server = 'https://api.telegram.org'
         tgurl = server + '/bot' + TG_BOT_TOKEN + '/sendMessage'
         rq_tg = requests.post(tgurl, data={'chat_id': TG_USER_ID, 'text': body}, headers={
